b1360d588d5dcbc59b79441026f1db266d45529d
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-12-06 10:53:53 +0900

    箱ひげか何かを作ろうと思ったが難しい

--- analyze.py
+++ analyze.py
@@ -1,25 +1,31 @@
-import bz2, io, sys
-from lxml import etree 
+import sys
+import numpy as np
+import matplotlib.pyplot as plot
 
 if __name__ == "__main__":
     path = sys.argv[1]
 
-    """
-    with-open-as構文で書こうと思ったけど
-    エラーが発生するため仕方なくxmlのハンドルは握ったままにする
-    """
-    try:
-        xml = etree.parse(path)
-        root = xml.getroot()
-    except:
-        print("Invalid Reading XML")
-        sys.exit()
-
-    print(root.tag)
-
-    try:
-        export_date = xml.xpath("/HealthData/ExportDate")[0].attrib["value"]
-    except:
-        print("HealthData has not ExportDate")
+    # あらかじめすべての行を展開する
+    with open(path, "r", encoding="UTF-8") as f:
+        lines = f.readlines()
     
-    print(export_date)
+    records = []
+
+    # データの整形を行う
+    for line in lines:
+        if len(line) <= 1:
+            continue
+        records.append(line.strip().split(";"))
+    
+    with open(path + ".re.csv", "w", encoding="SJIS") as f:
+        for record in records:
+            f.write(",".join(record) + "\n")
+
+    # numpyのDatetime型に変換
+    for record in records[1:]:
+        record[0] = record[0].replace(" ", "T")
+        record[1] = record[1].replace(" ", "T")
+
+        record[0] = np.datetime64(record[0])
+        record[1] = np.datetime64(record[1])
+    
